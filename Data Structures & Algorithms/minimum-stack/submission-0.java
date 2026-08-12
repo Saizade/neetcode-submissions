eda69636aed057b5d6157aec063e1ffc650e3b1d
Saizade <sai.zade01@gmail.com>
2026-08-12 22:46:09 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,43 @@
+class MinStack {
+
+    Stack<Integer> stack=new Stack<>();
+    Stack<Integer> minStack=new Stack<>();
+
+    // public MinStack() {
+    //     stack = new Stack<>();
+    //     minStack = new Stack<>();
+    // }
+
+    public void push(int val) {
+
+        stack.push(val);
+
+        if (minStack.isEmpty()) {
+            minStack.push(val);
+        } else {
+            int min = minStack.peek();
+
+            if (val < min) {
+                minStack.push(val);
+            } else {
+                minStack.push(min);
+            }
+        }
+    }
+
+    public void pop() {
+
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+
+        return stack.peek();
+    }
+
+    public int getMin() {
+
+        return minStack.peek();
+    }
+}
